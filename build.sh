c478604dad2420abca4f89a075f85a2c14c199dc
Ismael Luceno <ismael@initng.org>
2007-04-09 05:25:02 +0000

    Improved build.sh

--- build.sh
+++ build.sh
@@ -1,82 +1,64 @@
 #!/bin/sh
+[ -z "${BDIR}" ] && BDIR=build
 
-cleanup() {
-	cd ..
-	rm -rf build
+die()
+{
+	echo "ERROR: ${*}."
+	exit 1
 }
 
-echo "**** Creating the build directory ****"
-if [ -d build/ ] ; then
-	rm -rf build
+ask()
+{
+	echo -n "${*} [yes|no]? "
+	read _answer
+	[ "${_answer}" = "yes" ]
+}
+
+echo "**** Creating the build directory ***"
+if [ -d "${BDIR}" ]; then
+	echo "WARNING: ${BDIR} already exists."
+	ask "Do you want to continue" || exit 0
+	rm -rf "${BDIR}"
 fi
-mkdir build
-cd build
+mkdir "${BDIR}"
+cd "${BDIR}" || die "Can't create directory ${BDIR}"
 
+echo "**** Configuring project ****"
+cmake .. || die "Configuration failed"
 
-if [ $? -eq 0 ] ; then
-	echo "**** Configuring project ****"
-	cmake ..
-else
-	cleanup
-	exit 1
-fi
+echo "**** Compiling project ****"
+make || die "Compilation failed"
 
-
-if [ $? -eq 0 ] ; then
-	echo "Configuration successful."
-	echo "**** Compiling project ****"
-	make
-else
-	cleanup
-	exit 2
-fi
-
-if [ $? -eq 0 ] ; then
-	echo "Compilation successful."
-	echo "**** Installing project ****"
-	if [ $UID -eq 0 ] ; then
+echo "**** Installing project ****"
+if ask "Do you want to install initng now"; then
+	if [ ${UID} -eq 0 ]; then
 		make install
 	else	
 		echo "root privileges required."
 		su -c "make install"
-	fi
-else
-	cleanup
-	exit 3
+	fi || die "Installation failed"
 fi
 
-if [ $? -eq 0 ] ; then
-	echo "Installation successful."
-	echo "Reloading of initng is strongly recommended."
-	echo "I can reload initng (via ngc -R) for you [yes|no]:"
-	read answer
-	if [ x$answer = x"yes" ] ; then
-		echo "**** Reloading initng ****"
-		if [ $UID -eq 0 ] ; then
-			ngc -R
-		else
-			echo "root privileges required."
-			su -c "/sbin/ngc -R"
-		fi
+echo "Reloading of initng is strongly recommended."
+if ask "Do you want to reload initng now"; then
+	echo "**** Reloading initng ****"
+	if [ ${UID} -eq 0 ] ; then
+		ngc -R
 	else
-		echo "**** Skipping reloading of initng ****"
+		echo "root privileges required."
+		su -c "/sbin/ngc -R"
 	fi
 else
-	cleanup
-	exit 4
+	echo "**** Skipping reloading of initng ****"
+	echo "You can reload it calling 'ngc -R'."
 fi
 
-if [ $? -eq 0 ] ; then
-	# Calling count_me.sh from within the build directory (config.h)
-	echo "Do you want to be counted as user of initng [yes|no]:"
-	read answer
-	if [ x$answer = x"yes" ] ; then
-		../count_me.sh
-	fi
-	cleanup
-else
-	cleanup
-	exit 5
-fi
- 
+# Calling count_me.sh from within the build directory (config.h)
+ask "Do you want to be counted as user of initng" &&
+	../count_me.sh
+
+cd ..
+ask "Do you want to clear the build directory" &&
+	rm -rf "${BDIR}"
+
 exit 0
