4016ae5e53ddcbe00c0819db1bd6ca6f5bc9277f
Ismael Luceno <ismael@initng.org>
2008-04-13 09:36:16 +0000

    libinitng: reimplement initng_string_split_delim() to be more efficient

--- src/string/split_delim.c
+++ src/string/split_delim.c
@@ -21,6 +21,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+#define CHUNK	16
+
 /** Re-links a string to a argv-like string-array.
  * Example:   /bin/executable --ham -flt   --moo hoho  lalala\0
  *       =>   /bin/executable\0--ham\0-flt\0  --moo\0hoho\0 lalala\0
@@ -36,50 +39,73 @@
  * @got_it_working powerj
  * @improved_it ismael
  */
+
 char **initng_string_split_delim(const char *string, const char *delim,
-				 size_t * argc, int ofs)
+				 size_t *argc, int ofs)
 {
-	int len;
+	/* TODO: remove the argc and ofs arguments */
+
+	char *dest;
+	size_t len;
+
 	char **array;
+	size_t array_size;
 	size_t i = 0;
+
+	char **saveptr, *tok;
+	char *tmp;
 
 	if (!string)
 		return NULL;
 
-	array = (char **)initng_toolbox_calloc(1, sizeof(char *));
+	string += ofs; /* TODO: REMOVE */
 
-	while (string[ofs] != '\0') {
-		len = strcspn(string + ofs, delim);
-		if (len != 0) {
-			array = (char **)initng_toolbox_realloc(array,
-						sizeof(char *) * (i + 2));
-			array[i] = initng_toolbox_calloc(1, len);
-			memcpy(array[i], string + ofs, len);
-			i++;
-		} else {
-			len = 1;
-		}
-
-		ofs += len;
+	/* skip any inital character in delim, to make sure dest[0]
+	 * will be the start of the string */
+	while (strcspn(string, delim) == 0) {
+		string++;
 	}
 
-	array[i] = NULL;
+	len = strlen(string) + 1;
+	dest = initng_toolbox_calloc(1, len);
+	memcpy(dest, string, len);
 
-	if (argc)
-		*argc = i;
+	array = initng_toolbox_calloc(1, CHUNK * sizeof(char *));
+	array_size = CHUNK;
 
-	return array;
+	array[i++] = strtok_r(dest, delim, saveptr);
+	
+	while (tok = strtok_r(NULL, delim, saveptr) {
+		array[i++] = tok;
+		if (i % CHUNK == 0) {
+			array_size += CHUNK;
+			array = initng_toolbox_realloc(array, array_size *
+							      sizeof(char *));
+		}
+	}
+
+	array[i++] = NULL;
+
+	tmp = initng_toolbox_realloc(dest, len + i * sizeof(char *));
+	if (tmp != dest) {
+		for (int j = 0; i < i; j++) {
+			array[j] += tmp - dest;
+		}
+		dest = tmp;
+	}
+	
+	memcpy(&dest[len], array, i);
+
+	free(array);
+
+	if (argc) /* TODO: REMOVE */
+		*argc = i - 1;
+
+	return &dest[len];
 }
+
 
 void initng_string_split_delim_free(char **strs)
 {
-	int i;
-
-	if (!strs)
-		return;
-
-	for (i = 0; strs[i] != NULL; i++)
-		free(strs[i]);
-
-	free(strs);
+	free(strs[0]);
 }
