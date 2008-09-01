c3a9cd277ecb76c9ded52128bdd0d4464357f6ab
Ismael Luceno <ismael@initng.org>
2008-09-01 03:23:04 +0000

    lnitng: use a autotools-like config.h

--- config.h.cmake
+++
@@ -1,9 +0,0 @@
-#cmakedefine VERSION		"${VERSION}"
-#cmakedefine VERSION_NAME	"${VERSION_NAME}"
-#cmakedefine CHECK_RO
-#cmakedefine FORCE_NOCOLOR
-#cmakedefine FORCE_POSIX_IFILES
-#cmakedefine RUNLEVEL_DEFAULT	"${RUNLEVEL_DEFAULT}"
-#cmakedefine RUNLEVEL_FAKE	"${RUNLEVEL_FAKE}"
-
-#cmakedefine HAVE_ASPRINTF
