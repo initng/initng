dd4a8bef6d149f6e4b6623c2565308093fe85efd
Ismael Luceno <ismael@initng.org>
2009-07-25 05:32:20 -0300

    fix remaining files, missed in the GPLv2-to-GPLv3 transition

--- modules/initctl/initreq.h
+++ modules/initctl/initreq.h
@@ -6,7 +6,7 @@
  *              This library is free software; you can redistribute it and/or
  *              modify it under the terms of the GNU Lesser General Public
  *              License as published by the Free Software Foundation; either
- *              version 2 of the License, or (at your option) any later
+ *              version 3 of the License, or (at your option) any later
  *              version.
  *
  * Version:     @(#)initreq.h  1.28  31-Mar-2004 MvS
