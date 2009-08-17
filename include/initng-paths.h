582fe3d9d38411f802bd94e3319d821970068b76
Eric MSP Veith <eveith@wwweb-library.net>
2009-08-17 15:35:01 +0000

    Jamified build of libinitng.a

---
+++ include/initng-paths.h
@@ -0,0 +1,11 @@
+#ifndef INITNG_PATHS_H 
+#define INITNG_PATHS_H
+
+#define DEVDIR			"/dev"
+#define ETCDIR			"/etc"
+#define VARDIR			"/var"
+#define INITNG_ROOT		"/etc/initng"
+#define INITNG_PLUGIN_DIR	"/lib/initng"
+#define INITNG_CORE_BIN		"/sbin/initng-core"
+
+#endif /* INITNG_PATHS_H */
